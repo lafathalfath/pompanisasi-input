6d5aeb7aa1960c09f5389249830dbd6fb435c577
ardifm04 <ardifathul@gmail.com>
2024-08-12 15:14:59 +0700

    UPDATE:prov

--- resources/views/provinsi/abt/usulan.blade.php
+++ resources/views/provinsi/abt/usulan.blade.php
@@ -1,69 +1,63 @@
 @extends('layouts.provinsi')
 @section('content')
+<style>
+        .table thead th {
+        vertical-align: middle;
+        text-align: center;
+        background-color: #c8dce4;
+    }
 
-<head>
-    <style>
-        .content{
-        margin-left: 200px;
-    }
-    .table thead th {
-    vertical-align: middle;
-    text-align: center;
-    background-color: #c8dce4;
-    }
     .merged-cell {
-    background-color: #d9ead3;
-    text-align: center;
-    vertical-align: middle;
+        background-color: #d9ead3;
+        text-align: center;
+        vertical-align: middle;
     }
-    </style>
-</head>
-
 </style>
 <div class="d-flex flex-col justify-content-center">
     <div>
-        <!-- <div>
-            <a href="{{ route('kecamatan.abt.usulan.input') }}" type="submit" class="btn btn-success">Input Data</a>
-        </div><br> -->
+        <br>
         <div class="mb-3" style="display: flex; justify-content: space-between; gap: 10px; align-items: center;" >
             <i class="fa-solid fa-sliders"></i>
             <input type="date" class="form-control" id="date">
             <select name="kota_kabupaten" class="form-control" id="kota-kabupaten">
-                <option value="" disabled selected>Pilih Provinsi</option>
-                <option value="Jawa Barat">Jawa Barat</option>
-                <option value="Jawa Tengah">Jawa Tengah</option>
-                <!-- Tambahkan opsi Provinsi lainnya yang 1 wilayah -->
+                <option value="" disabled selected>Pilih Kecamatan</option>
+                <option value="Bogor Utara">Bogor Utara</option>
+                <option value="Bogor Selatan">Bogor Selatan</option>
+                <!-- Tambahkan opsi kota/kabupaten lainnya -->
             </select>
         </div>
         <table class="w-100 table table-bordered">
             <thead>
                 <tr>
-                    <th rowspan="2">No</th>
-                    <th rowspan="2">Kabupaten</th>
-                    <th rowspan="2">Kelompok tani</th>
-                    <th rowspan="2">Luas lahan (ha)</th>
-                    <th colspan="3" class="text-center">Pompa Refocusing Usulan</th>
-                    <th rowspan="2">No HP Poktan (jika ada)</th>
-                    <th rowspan="2">Aksi</th>
+                    <th>No</th>
+                    <th>Kabupaten</th>
+                    {{-- <th>Tanggal</th> --}}
+                    <th>Kelompok tani</th>
+                    <th>Luas lahan (ha)</th>
+                    <th class="text-center">Usulan Pompa ABT</th>
+                    {{-- <th>No HP Poktan (jika ada)</th> --}}
+                    <th>Aksi</th>
+                    {{-- <th>Total diusulkan (unit)</th> --}}
                 </tr>
-                <tr>
+                {{-- <tr>
                     <th>3 inch (unit)</th>
                     <th>4 inch (unit)</th>
                     <th>6 inch (unit)</th>
-                </tr>
+                </tr> --}}
             </thead>
             <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Bogor</td>
-                    <td>Kelompok tani 1</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>08123456789</td>
-                    <td><a href="{{ route('provinsi.detailkabupaten') }}" class="btn btn-sm btn-info">Detail</a></td>
-                </tr>
+                {{-- @forelse ($abt_usulan as $au) --}}
+                    <tr>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        {{-- <td><a href="{{ route('kabupaten.pompa.abt.usulan.detail', Crypt::encryptString($au->kecamatan->id)) }}" class="btn btn-sm btn-info">Detail</a></td> --}}
+                    </tr>
+                {{-- @empty --}}
+                    <tr><td colspan="6" class="text-center">Belum ada data</td></tr>
+                {{-- @endforelse --}}
             </tbody>
         </table>
 
