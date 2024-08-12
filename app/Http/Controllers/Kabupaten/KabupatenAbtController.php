ceb1d6e34bea425da81ef38d8e3e092a3ddfcf34
lafathalfath <iqnaraidan12@gmail.com>
2024-08-12 14:43:17 +0700

    UPDATE: detail detail kabupaten

--- app/Http/Controllers/Kabupaten/KabupatenAbtController.php
+++ app/Http/Controllers/Kabupaten/KabupatenAbtController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Kabupaten;
 
 use App\Http\Controllers\Controller;
+use App\Models\Desa;
+use App\Models\Kecamatan;
+use App\Models\PompaAbtDimanfaatkan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Crypt;
 
 class KabupatenAbtController extends Controller
 {
@@ -38,6 +42,21 @@ class KabupatenAbtController extends Controller
         return view('kabupaten.abt.Usulan', ['kecamatan' => $kecamatan, 'abt_usulan' => $abt_usulan]);
     }
 
+    public function detailUsulanView($kec_id) {
+        $kecamatan = Kecamatan::find(Crypt::decryptString($kec_id));
+        $desa = [];
+        $abt_usulan = [];
+        if ($kecamatan) {
+            $desa = $kecamatan->desa;
+            foreach ($desa as $des) {
+                foreach ($des->pompanisasi as $pom) {
+                    if ($pom->pompa_abt_usulan) $abt_usulan[] = $pom->pompa_abt_usulan;
+                }
+            }
+        }
+        return view('kabupaten.abt.abt_detail_kecamatan_usulan', ['desa' => $desa, 'abt_usulan' => $abt_usulan]);
+    }
+
     public function diterimaView() {
         $user = Auth::user();
         $kecamatan = [];
@@ -66,6 +85,21 @@ class KabupatenAbtController extends Controller
             }
         }
         return view('kabupaten.abt.Diterima', ['kecamatan' => $kecamatan, 'abt_diterima' => $abt_diterima]);
+    }
+
+    public function detailDiterimaView($kec_id) {
+        $kecamatan = Kecamatan::find(Crypt::decryptString($kec_id));
+        $desa = [];
+        $abt_diterima = [];
+        if ($kecamatan) {
+            $desa = $kecamatan->desa;
+            foreach ($desa as $des) {
+                foreach ($des->pompanisasi as $pom) {
+                    if ($pom->pompa_abt_usulan && $pom->pompa_abt_usulan->pompa_abt_diterima) $abt_diterima[] = $pom->pompa_abt_usulan->pompa_abt_diterima;
+                }
+            }
+        }
+        return view('kabupaten.abt.abt_detail_kecamatan_diterima', ['desa' => $desa, 'abt_diterima' => $abt_diterima]);
     }
 
     public function digunakanView() {
@@ -100,5 +134,25 @@ class KabupatenAbtController extends Controller
             }
         }
         return view('kabupaten.abt.Digunakan', ['kecamatan' => $kecamatan, 'abt_digunakan' => $abt_digunakan]);
+    }
+    
+    public function detailDigunakanView($kec_id) {
+        $kecamatan = Kecamatan::find(Crypt::decryptString($kec_id));
+        $desa = [];
+        $abt_dimanfaatkan = [];
+        if ($kecamatan) {
+            $desa = $kecamatan->desa;
+            foreach ($desa as $des) {
+                foreach ($des->pompanisasi as $pom) {
+                    if ($pom->pompa_abt_usulan && $pom->pompa_abt_usulan->pompa_abt_diterima && $pom->pompa_abt_usulan->pompa_abt_diterima->pompa_abt_dimanfaatkan) $abt_dimanfaatkan[] = $pom->pompa_abt_usulan->pompa_abt_diterima->pompa_abt_dimanfaatkan;
+                }
+            }
+        }
+        return view('kabupaten.abt.abt_detail_kecamatan_digunakan', ['desa' => $desa, 'abt_dimanfaatkan' => $abt_dimanfaatkan]);
+    }
+
+    public function detailDigunakanDetail($id) {
+        $abt_dimanfaatkan = PompaAbtDimanfaatkan::find(Crypt::decryptString($id));
+        return view('kabupaten.abt.detail_dimanfaatkan', ['abt_dimanfaatkan' => $abt_dimanfaatkan]);
     }
 }
